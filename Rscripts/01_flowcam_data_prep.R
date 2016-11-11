b610aa81a0f9f82c587e52910459fd2bc99e53d7
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2016-11-11 11:41:50 -0800

    updates to flow cam data cleaning script

--- Rscripts/01_flowcam_data_prep.R
+++ Rscripts/01_flowcam_data_prep.R
@@ -14,9 +14,9 @@ library(stringr)
 
 #### Step 1 #### 
 ## in the shell, use this command to copy the summary files from the folder of flow cam run folders to a summary-only file
-## cp **/*summary.csv /Users/Joey/Desktop/run-summaries
+## something like: cp **/*summary.csv /Users/Joey/Desktop/run-summaries
 
-## step 1b, read in UniqueID key
+## step 1b, read in UniqueID key (if there is one)
 
 # Unique_ID_key <- read.csv("/Users/Joey/Documents/chlamy-ktemp/k-temp/data-raw/PK-temp-UniqueID-key.csv")
 
@@ -28,9 +28,13 @@ cell_files <- list.files("/Users/Joey/Documents/J-TEMP/data-raw/flowcam-summarie
 names(cell_files) <- cell_files %>% 
 	gsub(pattern = ".csv$", replacement = "")
 
+
+#### Step 3: read in all the files!
+
 all_cells <- map_df(cell_files, read_csv, col_names = FALSE,
 										.id = "file_name")
 
+#### Step 4: pull out just the data we want, do some renaming etc.
 
 Jtemp <- all_cells %>% 
 	rename(obs_type = X1,
@@ -42,10 +46,10 @@ Jtemp <- all_cells %>%
 				 cell_density = `Particles / ml`,
 				 cell_volume = `Volume (ABD)`)
 
+#### Step 5: deal with the date times and separate the replicate names
 Jtemp$start.time <- ymd_hms("2016-11-08 16:15:43")
 
 Jtemp$time_since_innoc <- interval(Jtemp$start.time, Jtemp$start_time)
-
 
 
 Jtemp_all <- Jtemp %>% 
@@ -59,5 +63,5 @@ Jtemp_all <- Jtemp %>%
 	select(-other)
 
 
-### write out the correct csv file!! yay!
+#### Step 6: write out the correct csv file!! yay!
 write_csv(Jtemp_all, "data-processed/Jtemp_all.csv")
