b2341fc73e8a136c763439e0907950da02bab2c8
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2016-11-25 17:23:15 -0800

    adding the plotting script for Jane

--- Rscripts/02_exploration_plotting.R
+++ Rscripts/02_exploration_plotting.R
@@ -1,9 +1,23 @@
 #### plotting the J-TEMP data
 
 library(tidyverse)
+library(plotrix)
 
-jtemp <- read_csv("data-processed/Jtemp_all.csv")
+jtemp <- read_csv("data-processed/Jtemp_CR_all.csv")
 
 jtemp %>%
-	group_by(temperature) %>% 
-	ggplot(aes(x = time_since_innoc_hours, y = cell_density, color = factor(temperature))) + geom_point()
+	filter(species == "CR", temperature != 35) %>% 
+	group_by(temperature, rep) %>%
+	ggplot(aes(x = time_since_innoc_hours, group = rep, y = cell_density, color = factor(temperature))) + geom_point(size = 4) +
+	geom_line() + 
+	facet_wrap( ~ temperature, scales = "free")
+
+jtemp %>% 
+	filter(temperature == 8, species == "CR") %>%
+	group_by(rep) %>% 
+	ggplot(data = ., aes(x = time_since_innoc_hours, group = factor(rep), y = cell_density)) +
+	# geom_point(size = 4) +
+	geom_point() + 
+	geom_line() +
+	facet_wrap( ~ temperature)
+	
