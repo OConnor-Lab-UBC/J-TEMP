d15264513fe97fdcb6ab00775b431027e3ddc4d3
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2016-12-10 13:36:13 -0800

    estimating K via mm and SSlogis

--- Rscripts/02_exploration_plotting.R
+++ Rscripts/02_exploration_plotting.R
@@ -54,7 +54,7 @@ sea$cell_density[sea$time_since_innoc_days < 1] <- 2200 ## just setting all the 
 
 sea %>%
 	filter(temperature != "18") %>% 
-	filter(species == "TT") %>% 
+	filter(species == "CH") %>% 
 	group_by(temperature, rep) %>%
 	ggplot(aes(x = time_since_innoc_days, group = rep, y = cell_density, color = factor(temperature))) + geom_point(size = 4) +
 	geom_line() + 
@@ -68,17 +68,9 @@ write_csv(sea, "data-processed/sea.csv")
 all_species <- bind_rows(jtemp, sea)
 
 
-### let's go in and fix the time zero cell densities
-
-sea2 <- sea %>% 
-	mutate(cell_density = ifelse(time_since_innoc_days < 1, 2200, cell_density)) %>%
-	arrange(time_since_innoc_days)
-	
-
-
 # plot it all! ------------------------------------------------------------
 
-sea2 %>%
+sea %>%
 	filter(cell_density != 25415) %>% 
 	filter(cell_density != 36425) %>% 
 	# filter(species %in% c("CH", "TT", "CR"), temperature != 35) %>% 
@@ -90,10 +82,23 @@ sea2 %>%
 	unite(month_day, month, day, year) %>%
 	mutate(month_day = mdy(month_day)) %>% 
 	group_by(rep) %>%
-ggplot(aes(x = time_since_innoc_hours, y = cell_density, group = rep, color = factor(temperature))) +
+ggplot(aes(x = time_since_innoc_days, y = cell_density, group = rep, color = factor(temperature))) +
 	geom_line() + 
 	geom_point() +
 	facet_wrap( ~ temperature)
+
+
+sea %>%
+	filter(cell_density != 25415) %>% 
+	filter(cell_density != 36425) %>% 
+	filter(temperature != "35") %>% 
+	# filter(species == "CH") %>%
+	filter(time_since_innoc_days > 15) %>%
+	group_by(species, temperature, rep) %>% 
+	summarise(mean_density = mean(cell_density)) %>% 
+	ggplot(aes(x = temperature, y = mean_density)) + geom_point() +
+	geom_smooth() + facet_wrap( ~ species)
+
 	
 
 
@@ -127,28 +132,250 @@ geom_point(size = 5) +
 
 # estimate and plot r and K -----------------------------------------------
 
-all_species %>% 
-	filter(species == "CH") %>%
-	filter(temperature == "38") %>% 
-	filter(time_since_innoc_days > 0) %>% 
+mod <- sea %>% 
+	filter(species == "TT") %>%
+	# filter(temperature == "5") %>% 
 	group_by(temperature) %>%  
-	do(tidy(nls(cell_density ~ SSlogis(time_since_innoc_days, K, xmid, r), data = .))) %>% 
+	do(tidy(x = nls(cell_density ~ SSlogis(time_since_innoc_days, K, xmid, r), data = .), conf.int = TRUE, conf.level = 0.95)) %>% 
 	group_by(temperature) %>% 
-	filter(term == "K") %>% View
+	filter(term == "K") %>% 
 	# summarise_each(funs(mean, std.error), estimate) %>% View
 	ggplot(data = ., aes(x = temperature, y = estimate)) + geom_point(size = 4) +
 	scale_y_log10()
 
 
+sea1 <- sea %>% 
+	filter(time_since_innoc_days < 40)
 
-+ facet_wrap( ~ term, scales = "free") +
-	geom_errorbar(aes(ymin = estimate - std.error, ymax= estimate + std.error))
+mod5 <- sea %>% 
+	filter(species == "TT") %>%
+	filter(temperature == "5") %>% 
+	filter(time_since_innoc_days > 0) %>% 
+	group_by(rep) %>% 
+	do(tidy(nls(cell_density ~ SSlogis(time_since_innoc_days, K, xmid, r), data = .))) %>% 
+	# tidy(x = .,  conf.int = TRUE) %>% 
+	mutate(temperature = 5)
+
+mod8 <- sea %>% 
+	filter(species == "TT") %>%
+	filter(temperature == "8") %>% 
+	filter(time_since_innoc_days > 0) %>% 
+	group_by(rep) %>% 
+	do(tidy(nls(cell_density ~ SSlogis(time_since_innoc_days, K, xmid, r), data = .))) %>%
+	mutate(temperature = 8)
+
+mod16 <- sea %>% 
+	filter(species == "TT") %>%
+	filter(temperature == "16") %>% 
+	filter(time_since_innoc_days > 0) %>% 
+	group_by(rep) %>% 
+	do(tidy(nls(cell_density ~ SSlogis(time_since_innoc_days, K, xmid, r), data = .))) %>%
+	mutate(temperature = 16)
+
+mod25 <- sea %>% 
+	filter(species == "TT") %>%
+	filter(temperature == "25") %>% 
+	filter(time_since_innoc_days > 0) %>% 
+	group_by(rep) %>% 
+	do(tidy(nls(cell_density ~ SSlogis(time_since_innoc_days, K, xmid, r), data = .))) %>%
+	mutate(temperature = 25)
+
+mod32 <- sea %>% 
+	filter(species == "TT") %>%
+	filter(temperature == "32") %>% 
+	filter(time_since_innoc_days > 0) %>% 
+	group_by(rep) %>% 
+	do(tidy(nls(cell_density ~ SSlogis(time_since_innoc_days, K, xmid, r), data = .))) %>%
+	mutate(temperature = 32)
+
+mod38 <- sea %>% 
+	filter(species == "TT") %>%
+	filter(temperature == "38") %>% 
+	filter(rep != 5) %>% 
+	filter(time_since_innoc_days > 0) %>% 
+	group_by(rep) %>% 
+	do(tidy(nls(cell_density ~ SSlogis(time_since_innoc_days, K, xmid, r), data = .))) %>%
+	mutate(temperature = 38)
+
+
+mods <- bind_rows(mod5, mod8, mod16, mod25, mod38)
+
+mods %>% 
+	filter(term == "K") %>% 
+	filter(estimate < 50000) %>% 
+ggplot(data = ., aes(x = temperature, y = estimate)) + geom_point()
+
+
+mods %>% 
+	filter(term == "K") %>% 
+	filter(estimate < 50000) %>% 
+	mutate(inverse_temp = (1/(.00008617*(temperature+273.15)))) %>%
+	lm(log(estimate) ~ inverse_temp, data = .) %>% 
+	tidy(conf.int = TRUE)
+	summary
+
+
+mods %>% 
+	filter(term == "K") %>% 
+	mutate(inverse_temp = (1/(.00008617*(temperature+273.15)))) %>%
+	ggplot(aes(x = inverse_temp, y = log(estimate))) + geom_point(size = 4, alpha = 0.5, color = "#619CFF") +
+	geom_smooth(method = "lm", color = "#619CFF") +
+	scale_x_reverse() + xlab("temperature (1/kT)") + ylab("ln carrying capacity, K") + 
+	theme_minimal() + 
+	theme(axis.text.y   = element_text(size=20),
+				axis.text.x   = element_text(size=20),
+				axis.title.y  = element_text(size=20),
+				axis.title.x  = element_text(size=20),
+				panel.background = element_blank(),
+				panel.grid.major = element_blank(), 
+				panel.grid.minor = element_blank(),
+				axis.line = element_line(colour = "black"),
+				axis.ticks = element_line(size = 1)) +
+	theme(panel.border = element_blank(), axis.line = element_line(colour="black", size=1, lineend="square")) +
+	ylim(8.5, 11.5)
 
 
 
-nls(norm_div~K/(1+exp(r*norm_time)), data=df, 
-		start=list(K=1, r=-1.6), 
-		control=list(maxiter=1000, minFactor=.00000000001))
+mods %>% 
+	ungroup() %>% 
+	filter(term == "K") %>% 
+	mutate(inverse_temp = (1/(.00008617*(temperature+273.15)))) %>% 
+	do(tidy(lm(log(estimate) ~ inverse_temp, data = .), conf.int = TRUE)) %>% View
+
+
+
+# try to fit CHs with mm function -----------------------------------------
+
+sea %>% 
+	filter(species == "CH") %>% 
+	group_by(temperature, rep) %>% 
+	do(tidy(nls(cell_density ~ Vm * time_since_innoc_days/(K+time_since_innoc_days), data = ., 
+								 start = list(K = max(.$time_since_innoc_days)/2, Vm = max(.$cell_density))))) %>% 
+	filter(term == "Vm") %>% 
+	mutate(inverse_temp = (1/(.00008617*(temperature+273.15)))) %>% 
+	ungroup() %>% 
+	do(tidy(lm(log(estimate) ~ inverse_temp, data = .), conf.int = TRUE)) %>% View
+	
+sea %>% 
+	filter(species == "CH") %>% 
+	group_by(temperature, rep) %>% 
+	do(tidy(nls(cell_density ~ Vm * time_since_innoc_days/(K+time_since_innoc_days), data = ., 
+							start = list(K = max(.$time_since_innoc_days)/2, Vm = max(.$cell_density))))) %>% 
+	filter(term == "Vm") %>% 
+	mutate(inverse_temp = (1/(.00008617*(temperature+273.15)))) %>% 
+	ungroup() %>% 
+	ggplot(aes(x = inverse_temp, y = log(estimate))) + geom_point(size = 4, alpha = 0.5, color = "#619CFF") +
+	geom_smooth(method = "lm", color = "#619CFF") +
+	scale_x_reverse() + xlab("temperature (1/kT)") + ylab("ln carrying capacity, K") + 
+	theme_minimal() + 
+	theme(axis.text.y   = element_text(size=20),
+				axis.text.x   = element_text(size=20),
+				axis.title.y  = element_text(size=20),
+				axis.title.x  = element_text(size=20),
+				panel.background = element_blank(),
+				panel.grid.major = element_blank(), 
+				panel.grid.minor = element_blank(),
+				axis.line = element_line(colour = "black"),
+				axis.ticks = element_line(size = 1)) +
+				theme(panel.border = element_blank(), axis.line = element_line(colour="black", size=1, lineend="square"))
+
+
+?element_line
+
+data.s <- sea %>% 
+	filter(species == "CH") %>% 
+	rename(density = cell_density,
+				 day = time_since_innoc_days)
+
+data.s5 <- sea %>% 
+	filter(species == "CH") %>% 
+	filter(temperature == 5) %>% 
+	rename(density = cell_density,
+				 day = time_since_innoc_days)
+data.s8 <- sea %>% 
+	filter(species == "CH") %>% 
+	filter(temperature == 8) %>% 
+	rename(density = cell_density,
+				 day = time_since_innoc_days)
+data.s16 <- sea %>% 
+	filter(species == "CH") %>% 
+	filter(temperature == 16) %>% 
+	rename(density = cell_density,
+				 day = time_since_innoc_days)
+
+data.s25 <- sea %>% 
+	filter(species == "CH") %>% 
+	filter(temperature == 25) %>% 
+	rename(density = cell_density,
+				 day = time_since_innoc_days)
+
+data.s32 <- sea %>% 
+	filter(species == "CH") %>% 
+	filter(temperature == 32) %>% 
+	rename(density = cell_density,
+				 day = time_since_innoc_days)
+
+data.s38 <- sea %>% 
+	filter(species == "CH") %>% 
+	filter(temperature == 38) %>% 
+	rename(density = cell_density,
+				 day = time_since_innoc_days)
+
+
+model.nls5 <- nls(density ~ Vm * day/(K+day), data = data.s5, 
+									start = list(K = max(data.s5$day)/2, Vm = max(data.s5$density)))
+
+model.nls8 <- nls(density ~ Vm * day/(K+day), data = data.s8, 
+								 start = list(K = max(data.s8$day)/2, Vm = max(data.s8$density)))
+
+model.nls16 <- nls(density ~ Vm * day/(K+day), data = data.s16, 
+									start = list(K = max(data.s16$day)/2, Vm = max(data.s16$density)))
+
+model.nls25 <- nls(density ~ Vm * day/(K+day), data = data.s25, 
+									 start = list(K = max(data.s25$day)/2, Vm = max(data.s25$density)))
+
+model.nls32 <- nls(density ~ Vm * day/(K+day), data = data.s32, 
+									 start = list(K = max(data.s32$day)/2, Vm = max(data.s32$density)))
+
+model.nls38 <- nls(density ~ Vm * day/(K+day), data = data.s38, 
+									 start = list(K = max(data.s38$day)/2, Vm = max(data.s38$density)))
+
+
+mml5 <- data.frame(day = seq(0, max(400), length.out = 50))
+mml5$density <- predict(model.nls5, newdata = mml5)
+
+mml8 <- data.frame(day = seq(0, max(400), length.out = 50))
+mml8$density <- predict(model.nls8, newdata = mml8)
+
+mml16 <- data.frame(day = seq(0, max(400), length.out = 50))
+mml16$density <- predict(model.nls16, newdata = mml16)
+
+mml25 <- data.frame(day = seq(0, max(400), length.out = 50))
+mml25$density <- predict(model.nls25, newdata = mml25)
+
+mml32 <- data.frame(day = seq(0, max(400), length.out = 50))
+mml32$density <- predict(model.nls32, newdata = mml32)
+
+mml38 <- data.frame(day = seq(0, max(400), length.out = 50))
+mml38$density <- predict(model.nls38, newdata = mml38)
+
+
+ggplot(data.s, aes(x = day, y = density, color = factor(temperature))) +
+	theme_bw() +
+	xlim(0,400) + 
+	xlab("day") +
+	ylab("cell density") +
+	geom_point(alpha = 0.5, size = 6) +
+	geom_line(data = mml5, aes(x = day, y = density), colour = "#F8766D", size = 2) +
+	geom_line(data = mml8, aes(x = day, y = density), colour = "#B79F00", size = 2) +
+	geom_line(data = mml16, aes(x = day, y = density), colour = "#00BA38", size = 2) +
+	geom_line(data = mml25, aes(x = day, y = density), colour = "#00BFC4", size = 2) +
+	geom_line(data = mml32, aes(x = day, y = density), colour = "#619CFF", size = 2) +
+	geom_line(data = mml38, aes(x = day, y = density), colour = "#F564E3", size = 2)
+
+	
+library(scales)
+(hue_pal()(6))
 
 
 
