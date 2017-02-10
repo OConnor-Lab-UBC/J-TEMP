846e366ce2ee0647638a9e0f210913a743457940
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2017-02-09 21:19:30 -0800

    adding figures and fits

--- Rscripts/05_fitting.R
+++ Rscripts/05_fitting.R
@@ -1,18 +1,44 @@
-
+library(tidyverse)
 library(simecol)
+library(plotrix)
+library(gridExtra)
+library(stringr)
+
+
+
+# bring in the data -------------------------------------------------------
+
+jtemp <- read_csv("data-processed/Jtemp_all.csv")
+sea_raw <- read_csv("data-processed/sea.csv")
+
+sea <- sea_raw %>% 
+	mutate(total_biovolume = ifelse(is.na(total_biovolume), cell_density*cell_volume, total_biovolume)) %>% 
+	filter(cell_volume != 1776.750) %>% 
+	filter(cell_volume != 1893.750) %>% 
+	filter(cell_volume != 2453.760) %>% 
+	filter(cell_volume != 528.400) %>% 
+	filter(cell_volume != 581.380) %>% 
+	filter(cell_volume != 703.270) %>% 
+	filter(cell_volume != 398.070) %>% 
+	filter(cell_density > 2100) ## get rid of potentially erroneous measurement points that are really low
+
+	
+sea %>% 
+	filter(species == "CH", temperature == 38) %>% View
+
+sea %>% 
+	filter(species == "CH", temperature == 38) %>% 
+	group_by(rep) %>% 
+	# filter(time_since_innoc_days < 10) %>% 
+	ggplot(aes(x = time_since_innoc_days, y = total_biovolume, color = factor(rep))) + geom_point(size = 2)
+
+sea %>% 
+	filter(species == "TT", time_since_innoc_days < 1) %>% 
+	group_by(rep, temperature) %>% 
+	summarise(mean_biovol = mean(total_biovolume))
+	
 
 # set up models -----------------------------------------------------------
-
-
-Parameters <- c(r = 0.05, K = 10 ^ 5)
-
-# Declare the parameters to be used as the bounds for the fitting algorithm
-LowerBound <- c(r = 0.015, K = 10 ^ 5)
-UpperBound <- c(r = 2, K = 10 ^ 13) 
-
-# Declare the "step size" for the PORT algorithm. 1 / UpperBound is recommended
-# by the simecol documentation.
-ParamScaling <- 1 / UpperBound
 
 CRmodel <- new("odeModel",
 							 main = function (time, init, parms) {
@@ -23,7 +49,7 @@ CRmodel <- new("odeModel",
 							 },
 							 parms = Parameters, # Trying Joey's empirically estimated parameters here.
 							 times = c(from = 0, to = 43, by = 0.1), # the time interval over which the model will be simulated.
-							 init = c(P = 2200),
+							 init = c(P = 971410.2),
 							 solver = "lsoda" #lsoda will be called with tolerances of 1e-9, as seen directly below. Default tolerances are both 1e-6. Lower is more accurate.
 )
 
@@ -54,7 +80,6 @@ controlfit <- function(data){
 
 plotsinglefit <- function(data){
 	
-	
 	init(CRmodel) <- c(P = data$P[1]) # Set initial model conditions to the biovolume taken from the first measurement day
 	obstime <- data$days # The X values of the observed data points we are fitting our model to
 	yobs <- select(data, P) # The Y values of the observed data points we are fitting our model to
@@ -80,28 +105,36 @@ plotsinglefit <- function(data){
 	parms(plotfittedCRmodel)[fittedparms] <- coef(fittedCRmodel)
 	
 	# set model parameters to fitted values and simulate again
-	times(plotfittedCRmodel) <- c(from=0, to=42, by=1)
+	times(plotfittedCRmodel) <- c(from=0, to=43, by=1)
 	ysim <- out(sim(plotfittedCRmodel, rtol = 1e-9, atol = 1e-9))
 	
 	# Form observed data into a dataframe; the simulated data are already in a dataframe
 	observeddata <- data.frame(obstime, yobs)
+	observeddata$type <- "observed"
+	observeddata <- rename(observeddata, time = obstime)
 	simulateddata <- ysim
+	simulateddata$type <- "simulated"
+output <- bind_rows(observeddata, simulateddata)
 	
+
 	# Plot the results of our model fitting.
-	biol_plot <- ggplot() +
-		geom_point(data = observeddata, aes(x = obstime, y = yobs, color = "observed")) + # Observed data are points
-		geom_line(data = simulateddata, aes(x = time, y = P, color = "simulated")) + # Simulated data are in a continuous line
-		labs(x = "Time (days)", y = "Algal Biovolume")
+	# biol_plot <- ggplot() +
+	# 	geom_point(data = observeddata, aes(x = obstime, y = yobs, color = "observed")) + # Observed data are points
+	# 	geom_line(data = simulateddata, aes(x = time, y = P, color = "simulated")) + # Simulated data are in a continuous line
+	# 	labs(x = "Time (days)", y = "Algal Biovolume")
 	
 	# Output the results as a ggplot2 object
-	output <- biol_plot
+	# output <- biol_plot
 	return(output)
 }
 
 
 
+# SO ----------------------------------------------------------------------
+
+
 # fit and plot ------------------------------------------------------------
-Parameters <- c(r = 0.005, K = 10 ^ 5)
+Parameters <- c(r = 0.05, K = 10 ^ 5)
 
 # Declare the parameters to be used as the bounds for the fitting algorithm
 LowerBound <- c(r = 0.002, K = 10 ^ 2)
@@ -119,27 +152,27 @@ cells_so <- jtemp %>%
 	mutate(P = "low")
 
 
-all_cells <-	jtemp %>%
-	filter(species == "CR") %>% 
+all_cells_SO <-	jtemp %>%
+	filter(species == "SO") %>% 
 	# filter(total_biovolume < 1000000000) %>% 
 	unite(Unique_ID, temperature, rep, remove = FALSE) %>% 
-	mutate(P = "low") %>% 
 	rename(replicate = rep) %>% 
 	select(Unique_ID, cell_density, start_time, temperature, time_since_innoc_days, replicate) %>% 
 	rename(P = cell_density) %>% 
 	arrange(Unique_ID) %>% 
 	# filter(replicate == 1) %>% 
 	rename(days = time_since_innoc_days) %>% 
-	rename(ID = Unique_ID) 
+	rename(ID = Unique_ID) %>%
+	arrange(days)
 
 
-controldata <- split(all_cells, f = all_cells$ID)
+controldata_SO <- split(all_cells_SO, f = all_cells_SO$ID)
 
-output <- controldata %>% 
+output_SO <- controldata_SO %>% 
 	map_df(controlfit)
 
 
-all_cells %>% 
+all_cells_SO %>% 
 	filter(ID == "12_5") %>% 
 	plotsinglefit(.)
 
@@ -159,6 +192,21 @@ output %>%
 
 write_csv(output, "data-processed/SO_fitted_rK.csv")
 
+SO_fitted <- read_csv("data-processed/SO_fitted_rK.csv")
+
+SO_plot <- SO_fitted %>% 
+	filter(!grepl("NA", ID)) %>% 
+	separate(ID, into = c("temperature", "rep")) %>% 
+	mutate(temperature = as.numeric(temperature)) %>% 
+	filter(K < 10^7) %>% 
+	filter(r < 2) %>% 
+	# filter(temperature < 25) %>% 
+	ggplot(aes( x= temperature, y = K)) + geom_point(size = 3) + theme_bw() +
+	geom_smooth() +
+	ylab("carrying capacity (um3/ml)") + ggtitle("Scenedemus obliquus")
+	
+	
+
 
 
 # now moving onto other species here --------------------------------------
@@ -166,48 +214,170 @@ write_csv(output, "data-processed/SO_fitted_rK.csv")
 sea <- read_csv("data-processed/sea.csv")
 
 
-all_cells_sea <-	sea %>%
+all_cells_TT <-	sea %>%
 	filter(species == "TT") %>% 
 	# filter(total_biovolume < 1000000000) %>% 
 	unite(Unique_ID, temperature, rep, remove = FALSE) %>% 
 	rename(replicate = rep) %>% 
-	select(Unique_ID, cell_density, start_time, temperature, time_since_innoc_days, replicate) %>% 
-	rename(P = cell_density) %>% 
+	select(Unique_ID, cell_density, start_time, temperature, time_since_innoc_days, replicate, total_biovolume) %>% 
+	rename(P = total_biovolume) %>% 
 	arrange(Unique_ID) %>% 
 	# filter(replicate == 1) %>% 
 	rename(days = time_since_innoc_days) %>% 
-	rename(ID = Unique_ID) 
+	rename(ID = Unique_ID) %>% 
+	filter(!grepl("NA", ID)) %>% 
+	arrange(days)
 
-
+max(all_cells_sea$days)
 ggplot(data = all_cells_sea, aes(x = days, y = P, color = ID)) + geom_point() + geom_line()
 
 # fit and plot ------------------------------------------------------------
-Parameters <- c(r = 0.5, K = 10 ^ 5)
+Parameters <- c(r = 1, K = 10 ^ 7)
 
 # Declare the parameters to be used as the bounds for the fitting algorithm
-LowerBound <- c(r = 0.2, K = 10 ^ 2)
-UpperBound <- c(r = 10, K = 10 ^ 5) 
+LowerBound <- c(r = 0.01, K = 10 ^ 2)
+UpperBound <- c(r = 10, K = 10 ^ 9) 
 
 # Declare the "step size" for the PORT algorithm. 1 / UpperBound is recommended
 # by the simecol documentation.
 ParamScaling <- 1 / UpperBound
 
 
-controldata_sea <- split(all_cells_sea, f = all_cells_sea$ID)
-
+controldata_TT <- split(all_cells_TT, f = all_cells_TT$ID)
+str(controldata_sea)
 output_sea <- controldata_sea %>% 
 	map_df(controlfit)
 
+fitted_rk_TT <- output_sea
 
-all_cells_sea %>% 
-	filter(ID == "16_2") %>% 
-	plotsinglefit(.)
+## something weird is going on with the 38s!
+write_csv(fitted_rk_TT, "data-processed/fitted_rk_TT.csv")
+
+all_data <- controldata_TT %>% 
+	map_df(plotsinglefit, .id = "ID")
+
+all_data %>% 
+	separate(ID, into = c("temperature", "rep"), remove = FALSE) %>% 
+	mutate(temperature = as.numeric(temperature)) %>% View
+
+
+
+# Plot the results of our model fitting.
+all_data <- all_data %>% 
+	mutate(type = str_replace(type, "simulated", "fit"))
+
+ggplot() +
+	geom_point(data = all_data, aes(x = time, y = P, color = type)) + 
+	facet_wrap( ~ ID) + theme_bw() + ylab("biovolume (um3/ml)") + xlab("days")
+ggsave("figures/time_series_fits_TT.png", width = 12, height = 10)
+	
+	# Observed data are points
+	geom_line(data = simulateddata, aes(x = time, y = P, color = "simulated")) + # Simulated data are in a continuous line
+	labs(x = "Time (days)", y = "Algal Biovolume")
+
+
 
 output_sea %>% 
 	separate(ID, into = c("temperature", "rep")) %>% 
 	mutate(temperature = as.numeric(temperature)) %>% 
-	filter(K < 50000) %>% 
-	# filter(r < 2) %>% 
-	# filter(temperature < 25) %>% 
-	ggplot(aes( x= temperature, y = K)) + geom_point(size = 3) + scale_y_log10()
+	filter(K < 50000000) %>% 
+	filter(r < 1) %>% 
+	filter(temperature < 33) %>% 
+	group_by(temperature) %>% 
+	summarise_each(funs(mean, std.error), r, K) %>% 
+	ggplot(aes( x= temperature, y = r_mean)) + geom_point(size = 3) + theme_bw() +
+	geom_errorbar(aes(ymin = r_mean - r_std.error, ymax = r_mean + r_std.error), width = 0.1) +
+	geom_smooth() +
+	ylab("intrinsic growth rate (r)") + ggtitle("T. tetrahele")
 
+
+
+output_sea %>% 
+	separate(ID, into = c("temperature", "rep")) %>% 
+	mutate(temperature = as.numeric(temperature)) %>% 
+	filter(K < 50000000) %>% 
+	filter(r < 1) %>% 
+	ggplot(aes(x = temperature, y = K)) + geom_point()
+
+
+# now onto CH -------------------------------------------------------------
+
+all_cells_sea_CH <-	sea %>%
+	filter(species == "CH") %>% 
+	# filter(total_biovolume < 1000000000) %>% 
+	unite(Unique_ID, temperature, rep, remove = FALSE) %>% 
+	rename(replicate = rep) %>% 
+	select(Unique_ID, cell_density, start_time, temperature, time_since_innoc_days, replicate, total_biovolume) %>% 
+	rename(P = total_biovolume) %>% 
+	arrange(Unique_ID) %>% 
+	# filter(replicate == 1) %>% 
+	rename(days = time_since_innoc_days) %>% 
+	rename(ID = Unique_ID) %>% 
+	filter(!grepl("NA", ID)) %>% 
+	arrange(days)
+
+# fit and plot ------------------------------------------------------------
+Parameters <- c(r = 1, K = 10 ^ 7)
+
+# Declare the parameters to be used as the bounds for the fitting algorithm
+LowerBound <- c(r = 0.01, K = 10 ^ 2)
+UpperBound <- c(r = 10, K = 10 ^ 9) 
+
+# Declare the "step size" for the PORT algorithm. 1 / UpperBound is recommended
+# by the simecol documentation.
+ParamScaling <- 1 / UpperBound
+
+
+controldata_sea_CH <- split(all_cells_sea_CH, f = all_cells_sea_CH$ID)
+str(controldata_sea)
+
+output_sea_CH <- controldata_sea_CH %>% 
+	map_df(controlfit)
+
+fitted_rk_CH <- output_sea_CH
+
+all_cells_sea_CH %>% 
+	filter(ID == "38_1") %>% 
+	plotsinglefit(.)
+
+CH_K_plot <- output_sea_CH %>% 
+	filter(ID != "38_1") %>%  ## this one has a bad fit, taking out for now
+	separate(ID, into = c("temperature", "rep")) %>% 
+	mutate(temperature = as.numeric(temperature)) %>% 
+	filter(K < 500000000) %>% 
+	# filter(r < 1) %>% 
+	# filter(temperature < 25) %>% 
+	ggplot(aes( x= temperature, y = K)) + geom_point(size = 3) + theme_bw() +
+	ylab("carrying capacity, K (um3/ml)") +
+	ggtitle("Chlamydomonas carrying capacity") +
+	geom_smooth()
+ggsave("figures/CH_K.png", width = 10, height = 8)
+
+
+fitted_rk_TT %>% 
+	separate(ID, into = c("temperature", "rep")) %>% 
+	mutate(temperature = as.numeric(temperature)) %>% 
+	filter(K < 50000000) %>% 
+	filter(r < 1) %>% 
+	# filter(temperature < 25) %>% 
+	ggplot(aes( x= temperature, y = K, color = factor(rep))) + geom_point(size = 3) + theme_bw() +
+	ylab("carrying capacity, K (um3/ml)") +
+	ggtitle("T. Tetrahele carrying capacity")
+ggsave("figures/TT_K.png", width = 10, height = 8)
+
+TT_K_plot <- fitted_rk_TT %>% 
+	separate(ID, into = c("temperature", "rep")) %>% 
+	mutate(temperature = as.numeric(temperature)) %>% 
+	filter(K < 50000000) %>% 
+	filter(r < 1) %>% 
+	filter(temperature < 35) %>% 
+	ggplot(aes( x= temperature, y = K)) + geom_point(size = 3) + theme_bw() +
+	ylab("carrying capacity, K (um3/ml)") +
+	geom_smooth(method = lm) +
+	ggtitle("T. Tetrahele carrying capacity")
+
+
+
+all_K_combined_plot <- grid.arrange(TT_K_plot, CH_K_plot, SO_plot, nrow = 3)
+ggsave("figures/all_species_combined_K.png", all_K_combined_plot, width = 8, height = 10)
+?grid.arrange
