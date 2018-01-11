6826f8627f6bbae93a049d5c595d4ea0bba081d6
Jennifer Sunday <Sunday@biodiversity.ubc.ca>
2018-01-11 10:54:09 -0800

    some changes to some scripts

--- Rscripts/04_nitrate_plotting.R
+++ Rscripts/04_nitrate_plotting.R
@@ -8,7 +8,7 @@ with(CHNdata, plot(abs~temp))
 
 
 pdf("R-star_across_T.pdf", 6, 5)
-par(mfrow=c(2,2))
+par(mfrow=c(3,2))
 par(mar=c(3,4,1, 0.5), oma=c(1, 0, 1, 0))
 with(TTNdata, plot(nitrate~temp, ylim=c(0,15), xlab="", ylab="Nitrate remaining, umol", main="TT"))
 with(CHNdata, plot(nitrate~temp, ylim=c(0,15), xlab="", ylab="",  main="CH"))
@@ -26,4 +26,12 @@ segments(c(5,8,16,25,32,38),
 				 TTKmean$cell_density+TTKsd$cell_density,
 				 c(5,8,16,25,32,38),
 				 TTKmean$cell_density-TTKsd$cell_density)
+
+#For TT, take mean of data from 22 days onwards as approximation of K
+plot(TTKmean$cell_volume~levels(TTK$temperature), ylim=c(0, 30000), xlab="Temperature", ylab="")
+segments(c(5,8,16,25,32,38), 
+				 TTKmean$cell_volume+TTKsd$cell_volume,
+				 c(5,8,16,25,32,38),
+				 TTKmean$cell_volume-TTKsd$cell_volume)
 dev.off()
+names(TTKmean)
