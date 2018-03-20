d0d022b11479e350f709b469ccc6f0e2847375b1
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-03-20 14:28:38 -0700

    updates to biovolume to biomass conversions

--- Rscripts/18_biovol_biomass.R
+++ Rscripts/18_biovol_biomass.R
@@ -4,8 +4,8 @@
 ## cell dry mass = 0.47(volume)^0.99 (Reynolds 2006)
 ## cell dry mass =   -0.7550226 + 0.99(log V) (Reynolds 2006)
 ## strathmann: log C = -0.460 + 0.866(logV)
-##  C = 0.0109 * V ^ 0.991 (Montagnes et al. 1994, cited in Maranon  2008)
-##  logC = -4.518992 + 0.991(logV) (Montagnes et al. 1994, cited in Maranon  2008)
+##  C = 0.109 * V ^ 0.991 (Montagnes et al. 1994, cited in Maranon  2008)
+##  logC = -2.216407 + 0.991(logV) (Montagnes et al. 1994, cited in Maranon  2008)
 ## log C = -0.363 + 0.863 (log V) (Verity et al. 1992)
 ## from Menden Dueer -1.026 + 1.088 (log V) for chlorophytes
 ### 0.358(volume)^1.088 
@@ -27,25 +27,27 @@ tt <- read_csv("data-processed/TT_fit.csv")
 tt_mass <- tt %>% 
 	mutate(cell_biomass_MD = 0.3584378*(cell_volume)^1.088) %>% 
 	mutate(cell_biomass_R = 0.47*(cell_volume)^0.99) %>% 
-	mutate(population_biomass_MD = cell_biomass_MD * cell_density)
+	mutate(cell_biomass_M = 0.109 *(cell_volume)^0.991) %>% 
+	mutate(population_biomass_M = cell_biomass_M * cell_density)
 
 
 tt_mass %>% 
-	ggplot(aes(y = population_biomass_MD, x = days)) + geom_point() + 
+	ggplot(aes(y = population_biomass_M, x = days)) + geom_point() + 
 	facet_grid(temperature ~ rep)
 
 ## find starting biovolume
 
 tt_mass %>% 
 	filter(days < 1) %>% 
-	summarise(mean_biomass = mean(cell_biomass_MD)) %>% View
+	summarise(mean_biomass = mean(cell_biomass_M)) %>% View
 
 
-425.1173*2200
+
+68.75212*2200
 fits_many_biomass <- tt_mass %>% 
 	group_by(unique_id) %>% 
 	nest() %>% 
-	mutate(fit = purrr::map(data, ~ nls_multstart(population_biomass_MD ~ K/(1 + (K/935258.1 - 1)*exp(-r*days)),
+	mutate(fit = purrr::map(data, ~ nls_multstart(population_biomass_M ~ K/(1 + (K/151254.7 - 1)*exp(-r*days)),
 																								data = .x,
 																								iter = 500,
 																								start_lower = c(K = 100, r = 0),
@@ -53,7 +55,7 @@ fits_many_biomass <- tt_mass %>%
 																								supp_errors = 'N',
 																								na.action = na.omit,
 																								lower = c(K = 100, r = 0),
-																								upper = c(K = 50000000, r = 200),
+																								upper = c(K = 500000000, r = 200),
 																								control = nls.control(maxiter=1000, minFactor=1/204800000))))
 
 # get summary info
